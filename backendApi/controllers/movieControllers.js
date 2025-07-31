290a0644996bbcf9e778263479796649c49e03bb
Edoardo Lova <edoardolova81@gmail.com>
2025-07-31 16:23:10 +0200

    feat: add movieController, movieRouter and public

---
+++ backendApi/controllers/movieControllers.js
@@ -0,0 +1,42 @@
+const connection = require('../database/connection.js')
+
+function index(req, res){
+    const sql = 'SELECT * FROM movies';
+    connection.query(sql, (err, result)=>{
+        if (err) {
+            return res.status(500).json({error: err.message})
+        }
+
+        res.json(result)
+    })
+}
+
+function show (req,res){
+    const id = parseInt(req.params.id)
+    const movieSql = `SELECT * FROM movies WHERE id = ?`
+
+    connection.query(movieSql, [id], (err, result)=>{
+        if (err) {
+            return res.status(500).json({error: err.message})
+        }
+        if (result.length === 0) {
+            return res.status(404).json({mess: 'risorsa non trovata'})
+        }
+        const movie = result[0];
+
+        const reviewsSql = `SELECT * FROM reviews WHERE movie_id = ?`;
+        connection.query(reviewsSql, [id], (err, result)=>{
+            if (err) {
+                return res.status(500).json({error: err.message})
+            }
+
+            movie.reviews = result;
+            res.json(movie)
+        })
+    })
+}
+
+module.exports ={
+    index,
+    show
+}
---
+++ backendApi/routers/movieRouter.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router();
+
+const movieController = require('../controllers/movieControllers.js');
+
+// index
+router.get('/', movieController.index);
+
+// show
+router.get('/:id', movieController.show);
+
+module.exports = router;
